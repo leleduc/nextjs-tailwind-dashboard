b87c51ad96fb5b196c4babbca4ae939049131c24
leleduc <leleduc@gmail.com>
2023-07-28 15:53:30 +0700

    Final

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -5,25 +5,9 @@ Chart.register(...registerables);
 import { Bar } from 'react-chartjs-2';
 
 const options = {
-  // plugins: {
-  //   legend: {
-  //     display: false,
-  //   },
-  // },
   maintainAspectRatio: false,
   responsive: true,
-  // title: {
-  //   display: false,
-  //   text: 'Orders Chart',
-  // },
-  // tooltips: {
-  //   mode: 'index',
-  //   intersect: false,
-  // },
-  // hover: {
-  //   mode: 'nearest',
-  //   intersect: true,
-  // },
+
   legend: {
     labels: {
       fontColor: 'rgba(0,0,0,.4)',
@@ -31,43 +15,6 @@ const options = {
     align: 'end',
     position: 'bottom',
   },
-  // scales: {
-  //   xAxes: [
-  //     {
-  //       display: false,
-  //       scaleLabel: {
-  //         display: true,
-  //         labelString: 'Month',
-  //       },
-  //       gridLines: {
-  //         borderDash: [2],
-  //         borderDashOffset: [2],
-  //         color: 'rgba(33, 37, 41, 0.3)',
-  //         zeroLineColor: 'rgba(33, 37, 41, 0.3)',
-  //         zeroLineBorderDash: [2],
-  //         zeroLineBorderDashOffset: [2],
-  //       },
-  //     },
-  //   ],
-  //   yAxes: [
-  //     {
-  //       display: true,
-  //       scaleLabel: {
-  //         display: false,
-  //         labelString: 'Value',
-  //       },
-  //       gridLines: {
-  //         borderDash: [2],
-  //         drawBorder: false,
-  //         borderDashOffset: [2],
-  //         color: 'rgba(33, 37, 41, 0.2)',
-  //         zeroLineColor: 'rgba(33, 37, 41, 0.15)',
-  //         zeroLineBorderDash: [2],
-  //         zeroLineBorderDashOffset: [2],
-  //       },
-  //     },
-  //   ],
-  // },
 };
 
 export default function BarChart() {
@@ -122,7 +69,6 @@ export default function BarChart() {
           <div className="p-4 flex-auto">
             {/* Chart */}
             <div className="relative" style={{ height: '350px' }}>
-              {/* <canvas id="barChart"></canvas> */}
               <Bar data={data} options={options} />
             </div>
           </div>
